76029411a18676a57225fc67b7a15975af159f7c
Los diu <joaquinacosta2812@gmail.com>
2022-08-17 19:54:49 -0300

    initial version

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
-app-set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', function (request, response) {
     response.render('pages/home');
+});
+
+app.get('/productos', function (request, response){
+    response.render('pages/productos')
 });
 
 app.listen(3000);
