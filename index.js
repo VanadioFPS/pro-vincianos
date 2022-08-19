5bf27b440df2b019f38162529edeadddcff971ce
Los diu <joaquinacosta2812@gmail.com>
2022-08-18 22:49:15 -0300

    all pages ready

--- index.js
+++ index.js
@@ -7,8 +7,32 @@ app.get('/', function (request, response) {
     response.render('pages/home');
 });
 
-app.get('/productos', function (request, response){
-    response.render('pages/productos')
+app.get('/chat', function (request, response){
+    response.render('pages/chat')
+});
+
+app.get('/coaching', function (request, response){
+    response.render('pages/coaching')
+});
+
+app.get('/fixture', function (request, response){
+    response.render('pages/fixture')
+});
+
+app.get('/lineups', function (request, response){
+    response.render('pages/lineups')
+});
+
+app.get('/composiciones', function (request, response){
+    response.render('pages/composiciones')
+});
+
+app.get('/agentes', function (request, response){
+    response.render('pages/agentes')
+});
+
+app.get('/diccionario', function (request, response){
+    response.render('pages/diccionario')
 });
 
 app.listen(3000);
